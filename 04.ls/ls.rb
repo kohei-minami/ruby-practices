4ec27c92e1d7d7b7d17b5eb025229cd706f78bcf
koheiminami <kou373kou@gmail.com>
2023-03-28 23:45:16 +0900

    lsコマンド2の課題

---
+++ 04.ls/ls.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+files = Dir.glob('*', File::FNM_DOTMATCH)
+column = (files.count / 3.0).ceil
+sliced_files = files.each_slice(column).to_a
+max_size = sliced_files[0].size
+sliced_files.map! { |number| number.values_at(0...max_size) }
+transposed_files = sliced_files.transpose
+
+longest_file = files.max_by(&:size)
+
+transposed_files.each do |name|
+  name.each.with_index do |file, _file_index|
+    unless file.nil?
+      print file.ljust(longest_file.size + 5)
+    end
+  end
+  puts "\n"
+end
