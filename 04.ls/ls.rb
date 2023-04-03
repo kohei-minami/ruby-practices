cec49218e0ff59bf9f74e4bfb952b59333c6601a
koheiminami <kou373kou@gmail.com>
2023-04-03 23:14:13 +0900

    メソッド定義を加えた

--- 04.ls/ls.rb
+++ 04.ls/ls.rb
@@ -8,19 +8,26 @@ opt.parse!(ARGV)
 flags = params[:a] ? File::FNM_DOTMATCH : 0
 files = Dir.glob('*', flags)
 
-column = (files.count / 3.0).ceil
+def column_number
+  3.0
+end
+
+column = (files.count / column_number).ceil
 sliced_files = files.each_slice(column).to_a
 max_size = sliced_files[0].size
 sliced_files.map! { |number| number.values_at(0...max_size) }
 transposed_files = sliced_files.transpose
 
-longest_file = files.max_by(&:size)
+def new_line
+  puts "\n"
+end  
 
+longest_file = files.max_by(&:size)
 transposed_files.each do |name|
   name.each.with_index do |file, file_index|
     unless file.nil?
       print file.ljust(longest_file.size + 5)
     end
   end
-  puts "\n"
+  new_line
 end
